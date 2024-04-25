55a63406a0a271052ffa4c64c7388c28e0013746
João <joaoklippel.senai@gmail.com>
2024-04-25 16:29:39 -0300

    Folders cleaned

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-require('./startup/db')();
-require('./startup/routes')(app);
+require('./src/startup/db')();
+require('./src/startup/routes')(app);
 
 const port = 8000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
